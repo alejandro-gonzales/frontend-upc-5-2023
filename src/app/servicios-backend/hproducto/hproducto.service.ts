5f96defc0ffa2d7ff0823bd8d9771c0692d754aa
Alejandro Gonzales Viera <alejandro.g280211@gmail.com>
2023-06-20 21:31:15 -0400

    Update hproducto.service.ts

--- src/app/servicios-backend/hproducto/hproducto.service.ts
+++ src/app/servicios-backend/hproducto/hproducto.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HproductoService {
 
-  constructor() { }
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
+
+  URL_GET = this.PATH_BACKEND + "/api/HProducto"
+  URL_GET_BY_ID = this.PATH_BACKEND + "/api/HProducto/GetById"
+  URL_ADD_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/AddUsuario"
+
+  constructor(private http: HttpClient) { }
+
+  public GetHProducto(): Observable<HttpResponse<any>> {
+
+    return this.http
+        .get<any>(this.URL_GET,
+            { observe: 'response' })
+        .pipe();
+  }
+
+  public AddHProducto(entidad): Observable<HttpResponse<any>> {
+
+    return this.http
+        .post<any>(this.URL_ADD_HPRODUCTO, entidad,
+            { observe: 'response' })
+        .pipe();
+}
+
 }
