4bd2eb7dd0a3977d4b09557b77a28f2341de0d18
Alejandro Gonzales Viera <alejandro.g280211@gmail.com>
2023-06-22 23:11:40 -0400

    Revert "Update hproducto.service.ts"
    This reverts commit d5cf99cbfefb780c32e36041c9f30bfdf87648f3.

--- src/app/servicios-backend/hproducto/hproducto.service.ts
+++ src/app/servicios-backend/hproducto/hproducto.service.ts
@@ -1,9 +1,5 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< Updated upstream
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
-=======
->>>>>>> Stashed changes
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -14,7 +10,6 @@ export class HproductoService {
 
   PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
 
-<<<<<<< Updated upstream
   URL_GET_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto"
   URL_GET_BY_ID = this.PATH_BACKEND + "/api/HProducto/GetHProductoById"
   URL_ADD_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/AddHProducto"
@@ -57,28 +52,5 @@ export class HproductoService {
         {params: params, observe: 'response' })
       .pipe();
   }
-=======
-  URL_GET = this.PATH_BACKEND + "/api/HProducto"
-  URL_GET_BY_ID = this.PATH_BACKEND + "/api/HProducto/GetById"
-  URL_ADD_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/AddUsuario"
-
-  constructor(private http: HttpClient) { }
-
-  public GetHProducto(): Observable<HttpResponse<any>> {
-
-    return this.http
-        .get<any>(this.URL_GET,
-            { observe: 'response' })
-        .pipe();
-  }
-
-  public AddHProducto(entidad): Observable<HttpResponse<any>> {
-
-    return this.http
-        .post<any>(this.URL_ADD_HPRODUCTO, entidad,
-            { observe: 'response' })
-        .pipe();
-}
->>>>>>> Stashed changes
 
 }
